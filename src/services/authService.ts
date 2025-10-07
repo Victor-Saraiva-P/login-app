fa5de87c09eb44d47602037e2cceac90f07b95e4
Victor Saraiva <victoralex07062005@gmail.com>
2025-10-06 22:58:45 -0300

    Adiciona métodos getAccessToken e setAccessToken ao authService para gerenciamento de token de acesso

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -10,14 +10,36 @@ interface SignupRequestDTO extends LoginRequestDTO {
 
 const AUTH_CONTROLLER_PATH = "/auth";
 
+let accessToken: string | null = null;
+
 export const authService = {
+  getAccessToken: (): string | null => accessToken,
+
+  setAccessToken: (token: string): void => {
+    accessToken = token;
+  },
+
   signup: async (data: SignupRequestDTO) => {
     const response = await api.post(`${AUTH_CONTROLLER_PATH}/signup`, data);
+
+    accessToken = response.data.accessToken;
+
     return response.data;
   },
 
   login: async (data: LoginRequestDTO) => {
     const response = await api.post(`${AUTH_CONTROLLER_PATH}/login`, data);
+
+    accessToken = response.data.accessToken;
+
+    return response.data;
+  },
+
+  ping: async () => {
+    const response = await api.get(`${AUTH_CONTROLLER_PATH}/ping`);
+
+    console.log("Ping bem sucedido:", response.data);
+    
     return response.data;
   },
 };
